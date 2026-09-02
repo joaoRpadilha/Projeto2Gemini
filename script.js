44d6ef823aa9467ca462afe6e4016d9eb7010bee
joaoRpadilha <joao.renato.padilha@escola.pr.gov.br>
2026-09-02 08:17:59 -0300

    editei

--- script.js
+++ script.js
@@ -48,11 +48,15 @@ function diminuiFonte() {
 }
 // LEITURA DE TELA
 let lendo = false;
+
 const btnLeitura = document.getElementById("btnVoz");
+
 btnLeitura.addEventListener("click", lerEmVozAlta);
+
 function lerEmVozAlta() {
     let conteudo = document.querySelector("main");
 }
+
 function lerEmVozAlta() {
     let conteudo = document.querySelector("main");
     let texto = conteudo.innerText;
